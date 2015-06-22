0732fb782f097212f9ed2c2e829a042147d22bd8
David256 <davidborja456@gmail.com>
2015-06-22 18:40:23 -0500

    Se corrige el método enviar y se añade el método extraerEstado
    Se corrige el número de bytes que recibirá en el método Enviar, y se
    añade el método encargado de extraer el Estado

--- aybm.py
+++ aybm.py
@@ -7,7 +7,7 @@ import sys
 class ayb():
 
     def __init__(self):
-        self.HOST = 'isbanned.com'
+        self.HOST = 'http://isbanned.com'
 
     def enviar(self, unaUrl):
         #envia la peticion a la web
@@ -15,4 +15,16 @@ class ayb():
         datos = b'url=' + unaUrl + b'&4rme=Submit'
         urr = urllib.request.Request(self.HOST + '/index.php', datos)
         uru = urllib.request.urlopen(urr)
-        return uru.read(2048).decode('utf-8')
+        return uru.read(4096).decode('utf-8')
+
+    def extraerEstado(self, fuente):
+        inbuscar = 'id="myText" value="'
+        fibuscar = '">'
+        if fuente.find(inbuscar)!=-1 and fuente.find(fibuscar)!=-1:
+            pseudoInicio = fuente.find(inbuscar)
+            inicio = int(pseudoInicio) + len(inbuscar)
+            fin = fuente.find(fibuscar)
+            estado = fuente[inicio,fin]
+            return estado
+        else:
+            return False
